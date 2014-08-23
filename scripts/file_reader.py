905847d38182421bea5f097ccb5209f7c46dfa88
Chuan He <chuan_he@icloud.com>
2014-08-23 15:59:05 -0500

    sample datas

---
+++ scripts/file_reader.py
@@ -0,0 +1,17 @@
+import os
+
+def read_file(path):
+    f = open(path)
+    raw = f.read()
+    f.close()
+    return raw
+
+def read_folder(path):
+    raw = ""
+    for dir_entry in os.listdir(path):
+        dir_entry_path = os.path.join(path, dir_entry)
+        if os.path.isfile(dir_entry_path):
+            with open(dir_entry_path, 'r') as my_file:
+                raw += my_file.read()
+                my_file.close()
+    return raw
