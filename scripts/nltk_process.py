bc2dca5771812d365e5d4dc7432e846de3641df6
He Chuan <hechuan@hes-macbook-pro.dhcp.wustl.edu>
2014-08-23 11:24:33 -0500

    nltk_get_similirity_tree

--- scripts/nltk_process.py
+++ scripts/nltk_process.py
@@ -1,10 +1,36 @@
 import json
+import nltk
 
 from collections import Counter
+from nltk.corpus import *
 from nltk.tag import pos_tag
 
 def word_count(article, wordType, count=50):
-    tagged = pos_tag(article.split())
-    tagged_dic = [word for word, pos in tagged if pos == wordType]
-    hist = Counter(tagged_dic)
-    return json.dumps(hist.most_common(count))
+    tagged = pos_tag(nltk.regexp_tokenize(article.lower(), r'\w+', gaps=False))
+    tagged_dic = [word for word, pos in tagged if pos[0] == wordType]
+    hist = nltk.FreqDist(tagged_dic).items()[0:count]
+    return hist
+
+def get_similiarity(word1, word2):
+    try:
+        synset1 = wordnet.synsets(word1, pos=wordnet.NOUN)[0]
+        synset2 = wordnet.synsets(word2, pos=wordnet.NOUN)[0]
+        return wordnet.path_similarity(synset1, synset2)
+    except:
+        return 0
+
+def get_similar_words(original_word, wordList):
+    children = []
+    for word in wordList:
+        if (get_similiarity(original_word, word[0])) > 0.2 and (get_similiarity(original_word, word[0])) < 1:
+            children.append(word[0])
+    return {'name': original_word, 'children': children};
+    
+def build_similiarity_tree(original_word, wordList, levelRemains):
+    similiarity_tree = get_similar_words(original_word, wordList)
+    if levelRemains > 0:
+        children = []
+        for word in similiarity_tree['children']:
+            children.append(build_similiarity_tree(word, wordList, levelRemains-1));
+        similiarity_tree['children'] = children;
+    return similiarity_tree;
--- scripts/nltk_tester.py
+++ scripts/nltk_tester.py
@@ -1,3 +1,8 @@
 import nltk_process
+from nltk.corpus import *
 
-print nltk_process.word_count("Michael Jackson likes to eat at McDonalds McDonalds", "NNP");
+f = open('testfile.txt')
+raw = f.read()
+wordList =  nltk_process.word_count(raw, "N");
+print wordList
+print nltk_process.build_similiarity_tree("freedom", wordList, 2);
